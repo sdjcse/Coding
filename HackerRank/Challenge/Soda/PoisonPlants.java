7cdc798a3c560006d55c7bbeae79b06f41949913
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-02-21 07:41:05 -0700

    optimized poison plants

--- HackerRank/Challenge/Soda/PoisonPlants.java
+++ HackerRank/Challenge/Soda/PoisonPlants.java
@@ -1,13 +1,19 @@
 package com.soda.challenge;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by sdj on 2/18/17.
+ * https://www.hackerrank.com/challenges/poisonous-plants
  */
+class NodeP{
+    Long l;
+    int day;
+    NodeP(Long l,int day){
+        this.l=l;
+        this.day=day;
+    }
+}
 public class PoisonPlants
 {
     public static void main(String[] args)
@@ -16,27 +22,40 @@ public class PoisonPlants
         int n = inp.nextInt();
         List<Long> longList = new ArrayList<>();
         while(n>0){
-            longList.add(inp.nextLong());
+            longList.add(1*inp.nextLong());
             n--;
         }
         int counter = 0;
-        int i=longList.size()-1;
-        boolean removal = false;
+        Stack<NodeP> s = new Stack<>();
+        NodeP top = null;
+        int checker = 0;
+        for (int j = 0; j < longList.size(); j++)
+        {
+            if(s.isEmpty()){
+                s.push(new NodeP(longList.get(j),0));
+            }else{
+                top = s.peek();
+                if(longList.get(j) > top.l){
+                    s.push(new NodeP(longList.get(j),1));
+                }else{
+                    top = s.peek();
+                    checker = top.day;
+                    while (!s.isEmpty() && top.l>=longList.get(j)){
+                        s.pop();
+                        if(s.isEmpty())
+                            break;
+                        checker = Math.max(checker,top.day);
+                        top = s.peek();
+                    }
 
-        while(true){
-            i = longList.size()-1;
-            removal = false;
-            while(i>0){
-                if(longList.get(i)>longList.get(i-1)){
-                    removal = true;
-                    longList.remove(i);
+                    if(s.isEmpty()){
+                        s.push(new NodeP(longList.get(j),0));
+                    }else{
+                        s.push(new NodeP(longList.get(j),checker+1));
+                        counter = Math.max(counter,checker+1);
+                    }
                 }
-                i--;
             }
-            if(!removal){
-                break;
-            }
-            counter ++;
         }
         System.out.println(counter);
     }
