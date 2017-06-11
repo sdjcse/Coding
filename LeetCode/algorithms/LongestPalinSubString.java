bb03f2edf7ebd8d20c93d6d81f9956634f5919d5
sdjcse <sdjcse1@gmail.com>
2017-06-11 13:36:42 -0700

    palin substring

--- LeetCode/algorithms/LongestPalinSubString.java
+++ LeetCode/algorithms/LongestPalinSubString.java
@@ -7,10 +7,13 @@ public class LongestPalinSubString {
     public static void main(String[] args) {
         System.out.println(
                 new LongestPalinSubString().longestPalindrome(
-                        "babad"
+                        "ccc"
                 )
         );
     }
+
+    // Brute Force
+    /*
     public String longestPalindrome(String s) {
         int len = s.length();
         if( isPalindrome(
@@ -32,5 +35,36 @@ public class LongestPalinSubString {
 
     private boolean isPalindrome(String a){
         return new StringBuilder(a).reverse().toString().equals(a);
+    }
+    */
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int start=0,maxLen=1;
+        boolean determiner[][] = new boolean[n][n];
+        for (int i = 0; i < determiner.length; i++) {
+            for (int j = 0; j < determiner.length; j++) {
+                determiner[i][j] = i==j ;
+            }
+        }
+
+        for (int i = 0; i < determiner.length-1; i++) {
+            determiner[i][i+1] = s.charAt(i) == s.charAt(i+1);
+            if(determiner[i][i+1]){
+                start=i;
+                maxLen=2;
+            }
+        }
+
+        for (int i = 3; i <= determiner.length; i++) {
+            for (int j = 0; j < n-i+1; j++) {
+                int k = j+i-1;
+                determiner[j][k] = determiner[j+1][k-1] && s.charAt(j) == s.charAt(k);
+                if(determiner[j][k] && i>maxLen){
+                    start=j;
+                    maxLen=i;
+                }
+            }
+        }
+        return s.substring(start,start+maxLen);
     }
 }
